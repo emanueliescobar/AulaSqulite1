6458d82d1acba15abcb0c5938dd1a2078473e1da
matheus.neu <matheus.neu@ibiruba.ifrs.edu.br>
2019-05-20 08:29:26 -0300

    final

---
+++ resources/views/messages/list.blade.php
@@ -0,0 +1,34 @@
+<h1>Lista de Mensagens</h1>
+<hr>
+
+<!--Mensagem -->
+@if ($errors->any())
+	<hr>
+    <div class="container">
+        <div class="alert alert-success">
+			{{ \Session::get('success') }}
+        </div>
+    </div>
+	<hr>
+@endif
+
+<!--Mensagem -->
+@if (\Session::has('success'))
+	<hr>
+    <div class="container">
+        <div class="alert alert-success">
+			{{ \Session::get('success') }}
+        </div>
+    </div>
+	<hr>
+@endif
+
+@foreach($messages as $m)
+	<hr>
+	<h3>{{\Carbon\Carbon::parse($m->created_at)->format('d/m/Y h:m')}}</h3>
+	<p><a href="/messages/{{$m->id}}">{{$m->titulo}}</a></p>
+	<p>{{$m->texto}}</p>
+	<p>{{$m->autor}}</p>
+	<br>
+	<hr>
+@endforeach
